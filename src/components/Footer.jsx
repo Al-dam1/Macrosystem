ac4fe0132fad406e9ba28760c4af592e7d41053a
Damian Nicolas <damian.aldert@gmail.com>
2026-08-06 14:20:52 -0300

    Realice mejoras mobile en el footer , y mejore el Header con su Navbar, tambien mejore para mobile , falta detalles UX/UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className ="bg-gradient-to-b from-[#02060e] via-[#071e38] via-[#003165] via-[#004494] to-[#0356c5]">
-      <section className="grid items-start grid-cols-3 m-8 ">
+    <footer className="bg-gradient-to-b from-[#02060e] via-[#071e38] via-[#003165] via-[#004494] to-[#0356c5] ">
+      <section className="flex-col items-start grid-cols-3 m-8 md:grid sm:flex sm:justify-center">
         <div className="text-left">
           <h2 className="text-2xl text-amber-100 mb-[5px]">Categorias</h2>
           <ul>
@@ -25,7 +25,7 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="text-center">
+        <div className="md:text-center text-left mt-[12px]">
           <h2 className="text-2xl text-amber-100 mb-[5px]">Contactanos</h2>
           <ul>
             <li>
@@ -43,7 +43,7 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="text-right">
+        <div className="text-left mt-[12px] md:text-right">
           <h2 className="text-2xl text-amber-100 mb-[5px]">
             Sigamos conectados
           </h2>
@@ -54,44 +54,100 @@ const Footer = () => {
           </ul>
         </div>
       </section>
-     <div className="w-1/2 mx-auto mt-4 mb-4 border-t-4 border-blue-500"></div>
+      <div className="w-1/2 mx-auto mt-4 mb-4 border-t-4 border-blue-500"></div>
 
-      <section className="flex flex-col items-start p-4">
-      
-
+      <section className="flex-col items-start p-4 mx-auto md:flex ">
         <div className="flex flex-wrap items-center gap-4 mt-12">
           <h2>Medios de pago</h2>
-          <img src=" https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/visa@2x.png" className="h-12 "/>
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/mastercard@2x.png"className="h-12 " />
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/amex@2x.png" className="h-12 "/>
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/diners@2x.png" className="h-12 " />
+          <img
+            src=" https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/visa@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/mastercard@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/amex@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/diners@2x.png"
+            className="h-12 "
+          />
 
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/cabal@2x.png"className="h-12 " />
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/tarjeta-naranja@2x.png" className="h-12 "/>
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/tarjeta-shopping@2x.png"className="h-12 " />
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/other@2x.png" className="h-12 "/>
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/argencard@2x.png" className="h-12 "/>
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/cencosud@2x.png" className="h-12 "/>
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/cabaldebit@2x.png" className="h-12 "/>
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/maestro@2x.png" className="h-12 "/>
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/visadebit@2x.png" className="h-12 "/>
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/cabal@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/tarjeta-naranja@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/tarjeta-shopping@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/other@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/argencard@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/cencosud@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/cabaldebit@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/maestro@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/visadebit@2x.png"
+            className="h-12 "
+          />
 
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/pagofacil@2x.png" className="h-12 "/>
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/rapipago@2x.png" className="h-12 "/>
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/pagofacil@2x.png"
+            className="h-12 "
+          />
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/rapipago@2x.png"
+            className="h-12 "
+          />
         </div>
         <div className="flex items-center gap-2 mt-8">
           <h2>Medios de envío</h2>
-          <img src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/shipping/api/2682@2x.png" className="h-12 "/>
+          <img
+            src="https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/shipping/api/2682@2x.png"
+            className="h-12 "
+          />
         </div>
       </section>
-      <small className="flex w-full h-20 text-center bg-black align-center">
-        Copyright Macrosystem - 20443635115 - 2026. Todos los derechos
-        reservados. Defensa de las y los consumidores. Para reclamos{" "}
-        <a href="https://autogestion.produccion.gob.ar/consumidores">
-          ingresá acá.
-        </a>{" "}
-        / Botón de arrepentimiento
-      </small>
+     
+
+      <section  className="flex flex-col w-full p-4 text-left bg-black h-[auto] align-center gap-[3px]">
+        <small>
+          {" "}
+          Copyright Macrosystem - 20443635115 - 2026. Todos los derechos
+          reservados.
+        </small>
+        <br />
+        <small>
+          {" "}
+          Defensa de las y los consumidores. Para reclamos{" "}
+          <a href="https://autogestion.produccion.gob.ar/consumidores">
+            ingresá acá.
+          </a>{" "}
+          / Botón de arrepentimiento
+        </small>
+      </section>
     </footer>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,101 +1,146 @@
-import React from "react";
-import { User, ShoppingCart } from "lucide-react";
+import React, { useState } from "react";
+import { User, ShoppingCart, Menu, X } from "lucide-react";
 import Logo from "../../src/assets/canvas.png";
 import Mouse from "../../src/assets/mouse1.png";
 const Navbar = () => {
+  const [MenuMovil, setMenuMovil] = useState(false);
+  const [productosAbierto, setProductosAbierto] = useState(false);
   return (
-    <header>
-      <div className="contenedor">
-        <div className="contenido">
-          <img
-            src={Logo}
-            alt="logo de macrosystem"
-            width={"200px"}
-            height={"100px"}
-          />
-          <nav>
-            <ul className="flex gap-6">
-              <li className="active">Inicio</li>
-              <li className="relative group">
-                <a href="#" className="cursor-pointer">
-                  Productos
-                </a>
-                <ul className="absolute hidden p-2 mt-2 bg-gray-900 group-hover:block">
-                  <li>
-                    Conectividad
-                    <ul className="ml-4">
-                      <li>TP-Link</li>
-                      <li>Mercusys</li>
-                    </ul>
-                  </li>
-                  <li>Accesorios</li>
-                  <li>Impresión</li>
-                  <li>
-                    Periféricos
-                    <ul className="ml-4">
-                      <li>Teclados</li>
-                      <li>Mouse</li>
-                      <li>Combos</li>
-                      <li>Parlantes</li>
-                    </ul>
-                  </li>
-                  <li>
-                    Componentes de PC
-                    <ul className="ml-4">
-                      <li>Placas de video</li>
-                      <li>Procesadores</li>
-                      <li>Discos sólidos</li>
-                      <li>Memoria RAM PC</li>
-                      <li>Fuentes</li>
-                      <li>Motherboards</li>
-                      <li>Gabinetes</li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">Contacto</a>
-              </li>
-              <li>
-                <a href="#">Políticas de devolución</a>
-              </li>
-              <li>
-                <a href="#">Políticas de envío</a>
-              </li>
-            </ul>
-          </nav>
+    <>
+      <header>
+        <div className="contenedor">
+          <div className="contenido">
+            <img
+              src={Logo}
+              alt="logo de macrosystem"
+              width={"200px"}
+              height={"100px"}
+            />
+            <nav className="sticky z-50 ">
+              <ul className="hidden gap-6 ml-8 md:flex">
+                <li className="active">Inicio</li>
+                <li className="relative group">
+                  <a href="#" className="cursor-pointer">
+                    Productos
+                  </a>
+                  <ul className="absolute hidden p-2 mt-2 bg-gray-900 group-hover:block">
+                    <li>
+                      Conectividad
+                      <ul className="ml-4">
+                        <li>TP-Link</li>
+                        <li>Mercusys</li>
+                      </ul>
+                    </li>
+                    <li>Accesorios</li>
+                    <li>Impresión</li>
+                    <li>
+                      Periféricos
+                      <ul className="ml-4">
+                        <li>Teclados</li>
+                        <li>Mouse</li>
+                        <li>Combos</li>
+                        <li>Parlantes</li>
+                      </ul>
+                    </li>
+                    <li>
+                      Componentes de PC
+                      <ul className="ml-4">
+                        <li>Placas de video</li>
+                        <li>Procesadores</li>
+                        <li>Discos sólidos</li>
+                        <li>Memoria RAM PC</li>
+                        <li>Fuentes</li>
+                        <li>Motherboards</li>
+                        <li>Gabinetes</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Contacto</a>
+                </li>
+                <li>
+                  <a href="#">Políticas de devolución</a>
+                </li>
+                <li>
+                  <a href="#">Políticas de envío</a>
+                </li>
+              </ul>
+            </nav>
 
-          <div className="contenido-users">
-            <div className="user">
-              <User color="#c0bfbc" />
-              <span className="label">Mi cuenta</span>
-            </div>
-            <div className="cart">
-              <ShoppingCart color="#c0bfbc" />
-              <span className="label">Carrito</span>
+            <div className="contenido-users">
+              <div className="user">
+                <User color="#c0bfbc" />
+                <span className="label">Mi cuenta</span>
+              </div>
+              <div className="cart">
+                <ShoppingCart color="#c0bfbc" />
+                <span className="label">Carrito</span>
+              </div>
+              <button
+                className="p-2 md:hidden"
+                onClick={() => setMenuMovil(!MenuMovil)}>
+                {MenuMovil ? <X color="#c0bfbc" /> : <Menu color="#c0bfbc" />}
+              </button>
             </div>
           </div>
         </div>
-      </div>
 
-      <article className="flex flex-col items-center w-full mx-auto sm:flex-row sm:justify-between sm:items-start sm:w-full">
-        <div className="oferta sm:text-left sm:w-1/2">
-          <span className="oferta-span">Oferta del dia!</span>
-          <h2 className="oferta-h2">Mouse Gamer Rgb</h2>
-          <p className="oferta-description">
-            Precisión milimétrica y estilo gamer. Con iluminación RGB
-            personalizable, diseño ergonómico y botones programables, este mouse
-            te da ventaja en cada partida.
-          </p>
-          <a className="oferta-btn" href="#">
-            Comprar
-          </a>
+        <article className="flex flex-col items-center mx-auto sm:flex-row sm:justify-between sm:items-start">
+          <div className="oferta sm:text-left ">
+            <span className="oferta-span">Oferta del dia!</span>
+            <h2 className="oferta-h2">Mouse Gamer Rgb</h2>
+            <p className="oferta-description">
+              Precisión milimétrica y estilo gamer. Con iluminación RGB
+              personalizable, diseño ergonómico y botones programables, este
+              mouse te da ventaja en cada partida.
+            </p>
+            <a className="oferta-btn" href="#">
+              Comprar
+            </a>
+          </div>
+          <div className="flex items-center justify-center oferta-img sm:w-1/2">
+            <img src={Mouse} className="object-contain w-full h-64 mx-auto" />
+          </div>
+        </article>
+      </header>
+      {MenuMovil && (
+        <div className="fixed inset-0 z-50 flex flex-col bg-gray-900 shadow-2xl md:hidden">
+       
+          <div className="flex justify-end p-4">
+            <button
+              className="p-2 md:hidden"
+              onClick={() => setMenuMovil(!MenuMovil)}>
+              {" "}
+              {MenuMovil ? (
+                <X color="#c0bfbc" />
+              ) : (
+                <Menu color="#c0bfbc" />
+              )}{" "}
+            </button>
+          </div>
+
+          <ul className="flex flex-col gap-6 px-6 py-4 text-white">
+            
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Contacto</li>
+            <li>Políticas de devolución</li>
+            <li>Políticas de envío</li>
+            <div className="flex flex-row gap-8">  
+              <li className="flex flex-row items-center gap-2">
+              <User color="#c0bfbc" />
+              <span>Crear cuenta</span>
+            </li>
+            <li className="flex items-center gap-2">
+            
+              <span>Iniciar seccion</span>
+            </li></div>
+           
+          </ul>
         </div>
-        <div className="flex items-center justify-center oferta-img sm:w-1/2">
-          <img src={Mouse} className="object-contain w-full h-64 mx-auto" />
-        </div>
-      </article>
-    </header>
+      )}
+    </>
   );
 };
 
